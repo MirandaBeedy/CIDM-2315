73ad543add366a1b89885522d7e7e9bcb48e3d0d
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2024-01-22 00:55:02 -0600

    Homework1

---
+++ Homework1.cs.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       double x = 2.5;
+     double y = 3.3;
+     
+    Console.WriteLine(4*x*x+3*y);
+          
+    }
+}
