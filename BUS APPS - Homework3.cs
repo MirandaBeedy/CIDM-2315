2bfd8a5d454264395a926f113cd45333629db55b
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2024-02-14 21:49:58 -0600

    Add files via upload

---
+++ BUS APPS - Homework3.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+ {
+        Console.WriteLine("Enter a prime number: ");
+        var n = int.Parse(Console.ReadLine());
+        var prime = true;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(n + " is not a prime number");
+            }
+        }
+        if (n % 2 == 0)
+        {
+            Console.WriteLine(n + " is a prime number");
+        }
+    }
+}
