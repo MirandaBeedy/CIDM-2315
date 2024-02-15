15e010ff8bfda64c5f93a4170c22ffb06098b16c
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2024-02-14 21:18:39 -0600

    Add files via upload

---
+++ Bus Apps - Homework2.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework2;
+
+class Program
+ {
+    Console.WriteLine("Please input an letter grade: ");
+    string letter = Console.ReadLine();
+    switch(letter){
+        case "A":
+            Console.WriteLine("GPA is 4.");
+            break;
+        case "B":
+            Console.WriteLine("GPA is 3.");
+            break;
+        case "C":
+            Console.WriteLine("GPA is 2.");
+            break;
+        case "D":
+            Console.WriteLine("GPA is 1.");
+            break;
+        case "F":
+            Console.WriteLine("GPA is 0.");
+            break;
+        default:
+            Console.WriteLine("Wrong Letter Grade!");
+            break;
+    }
+  }
+}
