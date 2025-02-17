93eccbd47035ed7d5677764a56c71ed3a7c6d453
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2025-02-17 17:52:56 -0600

    Add files via upload

---
+++ Homework3prt2.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework3prt2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Assing a int value to N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int h = 0; h < N; h++)
+            {
+                 Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
