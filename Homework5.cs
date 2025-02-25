df14911b2f4216cf00d7fc4921bbc31a431869e3
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2025-02-24 21:38:04 -0600

    Add files via upload

---
+++ Homework5.cs
@@ -0,0 +1,26 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 9;
+        int num2 = 2;
+
+        int largest = FindLargest(num1, num2);
+        Console.WriteLine($"The largest number between {num1} and {num2} is: {largest}");
+    }
+
+    static int FindLargest(int num1, int num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;
+        }
+
+        else
+        {
+            return num2;
+        }
+    }
+}
