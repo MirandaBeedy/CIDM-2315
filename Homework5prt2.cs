0670a23192e7261f93f75dbb641e0e0051ec3c54
MirandaBeedy <135286405+MirandaBeedy@users.noreply.github.com>
2025-02-24 21:41:08 -0600

    Add files via upload

---
+++ Homework5prt2.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static int FindLargest(int num1, int num2, int num3, int num4)
+    {
+        int largest = num1;
+        if (num2 > largest) largest = num2;
+        if (num3 > largest) largest = num3;
+        if (num4 > largest) largest = num4;
+        return largest;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a second number:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a third number:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a fourth number:");
+        int num4 = int.Parse(Console.ReadLine());
+
+        int largest = FindLargest(num1, num2, num3, num4);
+        Console.WriteLine("The largest number is:" + largest);
+
+    }
+}  
